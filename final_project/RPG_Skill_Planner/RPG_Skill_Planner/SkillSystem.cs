35bff284d4442579c78b3ec522ff5b279d86902c
Miles Griffith <milesbgriffith@gmail.com>
2025-12-03 13:49:08 -0800

    fix trees

---
+++ final_project/RPG_Skill_Planner/RPG_Skill_Planner/SkillSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Skill_Planner
+{
+    public class SkillSystem
+    {
+        private static Dictionary<string, Skill> skillTree = new Dictionary<string, Skill>();
+        private int operationCount;
+        private DateTime sessionStart;
+
+        public SkillSystem()
+        {
+            operationCount = 0;
+            sessionStart = DateTime.Now;
+
+            Console.WriteLine("Skill System initialized.");
+        }
+
+    }
+}
